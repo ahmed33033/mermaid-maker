69264f441ab3b8abfb9d47120161ad02c5b81bbe
Ahmed <ahmed.akod@outlook.com>
2026-07-01 15:15:40 -0400

    refactor: remove js action files

--- src/file-collector.ts
+++
@@ -1,5 +0,0 @@
-import { action_input_type } from "./input-schema.js"
-
-async function file_collector(inputs: action_input_type) {
-    return
-}
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * The entrypoint for the action. This file simply imports and runs the action's
- * main logic.
- */
-import { input_collector } from "./input-collector.js"
-/* istanbul ignore next */
-input_collector()
